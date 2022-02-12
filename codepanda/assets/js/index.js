7ba6f2ac6aab332c3db1435f9c0a163263518432
pranshu verma <pranshu.verma98@gmail.com>
2022-02-12 15:56:25 +0530

    Version: 22.02.12.02: Few CSS Updates

--- codepanda/assets/js/index.js
+++ codepanda/assets/js/index.js
@@ -33,3 +33,31 @@ var flkty = new Flickity(elem, {
 var flkty = new Flickity('.carousel', {
   // options
 });
+
+
+ $("#carousel-multiple").on("slide.bs.carousel", function (e) {
+    var itemsPerSlide = parseInt($(this).attr('data-maximum-items-per-slide')),
+      totalItems = $(".carousel-item", this).length,
+      reserve = 1,//do not change
+      $itemsContainer = $(".carousel-inner", this),
+      it = (itemsPerSlide + reserve) - (totalItems - e.to);
+
+    if (it > 0) {
+      for (var i = 0; i < it; i++) {
+        $(".carousel-item", this)
+          .eq(e.direction == "left" ? i : 0)
+          // append slides to the end/beginning
+          .appendTo($itemsContainer);
+      }
+    }
+  });
+  
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar-button").style.visibility = "visible";
+  } else {
+	document.getElementById("navbar-button").style.visibility = "hidden";
+  }
+}
